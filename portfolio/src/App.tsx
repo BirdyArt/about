4a466a6f7dc465eb964f4fbc04619aa4d10d872e
BirdyArt <asobolev91@gmail.com>
2024-05-17 11:04:42 -0700

    test

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,13 +1,7 @@
-import { useState } from 'react'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div>
       <h1>Hello, world!</h1>
-      <button onClick={() => setCount(count + 1)}>Click me</button>
-      <p>Count: {count}</p>
     </div>
   )
 }
